a7c9806a659a125d64eff501f3e7ac3b33b9af25
Pieter Beulque <pieterbeulque@gmail.com>
2022-12-13 13:26:40 +0100

    Actually export `calculateHumanReadableNumber`

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
+export { default as calculateHumanReadableNumber } from './calculateHumanReadableNumber/calculateHumanReadableNumber';
 export { default as getIdentifier } from './getIdentifier/getIdentifier';
 export { default as parseHumanReadableNumber } from './parseHumanReadableNumber/parseHumanReadableNumber';
