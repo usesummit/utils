a1a7605fb66689801ad01df9fa35ac9d4f58fc1c
Pieter Beulque <pieterbeulque@gmail.com>
2023-02-15 13:50:29 +0100

    Add missing exports related to CookieStorage

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 export { default as calculateHumanReadableNumber } from './calculateHumanReadableNumber/calculateHumanReadableNumber';
 export { default as getCookieStorage } from './getCookieStorage/getCookieStorage';
+export type {
+    CookieOptions,
+    CookieStorageOptions,
+} from './getCookieStorage/getCookieStorage';
 export { default as getIdentifier } from './getIdentifier/getIdentifier';
 export { default as parseHumanReadableNumber } from './parseHumanReadableNumber/parseHumanReadableNumber';
